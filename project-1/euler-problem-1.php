7cc5fe2e391de7021d86e7f275582919c649cc35
Ron Hoffecker <qwixilver@gmail.com>
2017-01-31 09:33:19 -0600

    Okay updated everything in the PR. Now complete in just 7 lines woo woo

--- project-1/euler-problem-1.php
+++ project-1/euler-problem-1.php
@@ -6,33 +6,11 @@ The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
 //not trying to do any fancy tdd or function building right off here - just want to solve the problem fist =]
+$sum_of_multiples = 0;
 for ($i=0; $i < 1000; $i++) {
-  if ($i%5 == 0)
+  if ($i%5 == 0 || $i%3 == 0) // Thanks for the correction here!
   {
     $sum_of_multiples += $i;
   }
-  else if ($i%3 == 0)
-  {
-    $sum_of_multiples += $i;
-  }
-
 }
 echo "the sum of all multiples of 3 or 5 below 1000 is: " . $sum_of_multiples ."\n";
-
-
-/*comentary while I code this:
-
-What am I not understanding about boolean logic here? I tried a couple of things to get the sorting down to a single
-if statement as such:
-
-if ($i%5 || $i%3 == 0)
-{
-  $sum_of_multiples += $i;
-}
-
-I also tried using the text 'or' operator, as well as the 'xor' operator
-(though I figured that one would be issues with numbers like 15 which is a multiple of *both* 3 and 5)
-
-but in all cases trying to use boolean logic in an if statement like this, I seem to get bad results :(
-
-*/
